750356eaad9f245f74b36e26724dce48ba7a49d0
sunnysai12345 <sunnysai12345@gmail.com>
2015-12-09 21:19:33 +0530

    Update tweetbot.py

--- tweetbot.py
+++ tweetbot.py
@@ -1,9 +1,9 @@
 from twitter import *
 def tweetbot():
-    access_token = '396545890-p9hJW2yKDUyRIoSAllg7MKRsLiGoG79Ja9X4WqxI'
-    access_token_secret = 'CW4eJko1xogxWWZ6DQp2X1Q554QnJnz8ZBTJUHL93SVdq'
-    consumer_key ='KDI8fveXLwn5l7r1Vww11QIzd'
-    consumer_secret ='NmtTtWQhzQuu2TGq8vGDaM6EGz71JtW3ncWtt44QfA7rfU97fE'
+    access_token = ''
+    access_token_secret = ''
+    consumer_key =''
+    consumer_secret =''
     t = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
     searchfor=raw_input('enter string:')
     x=t.search.tweets(q=searchfor)
