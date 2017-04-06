94267fcc7b9347b879af91e904f06df65a82de71
Kondalarao Vonteru <sunnysai12345@gmail.com>
2017-04-06 10:13:58 +0530

    Update tweetbot.py
    Added Comments for better readability

--- tweetbot.py
+++ tweetbot.py
@@ -1,12 +1,15 @@
 from twitter import *
 def tweetbot():
+    #Fill the details below
     access_token = ''
     access_token_secret = ''
     consumer_key =''
     consumer_secret =''
     t = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
+    #Enter search string
     searchfor=raw_input('enter string:')
     x=t.search.tweets(q=searchfor)
+    #Set range for how many tweets u want to send
     for i in range(15):
         s=x['statuses'][i]['text']
         t.statuses.update(status=s)
